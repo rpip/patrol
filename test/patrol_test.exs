6d4c76458d83a4061eb3c0ebb57d59e4ff82ed65
Mawuli Adzaku <mawuli@mawuli.me>
2014-06-28 01:33:42 +0000

    Add more tests
    * creating custom sandbox
    * redirect IO to a file
    * use Standard IO for IO

--- test/patrol_test.exs
+++ test/patrol_test.exs
@@ -2,8 +2,39 @@ defmodule PatrolTest do
   use ExUnit.Case
   use Patrol
 
-  test "create sandbox module" do
+
+  setup do
     sb = Patrol.create_sandbox()
-    sb.("System.halt")
+
+    {:ok, [sandbox: sb]}
   end
+
+  test "create sandbox", ctx do
+    assert(is_function(ctx[:sandbox]))
+  end
+
+  test "sandbox simple addition is true", ctx do
+    assert {:ok, 4} == ctx[:sandbox].("1 + 3")
+  end
+
+  test "custom sandbox" do
+    sb = %Patrol.Sandbox{}
+    assert {:ok, _version} = Patrol.eval("System.version", sb)
+  end
+
+  test "sandbox use standard IO" do
+    sb = %Patrol.Sandbox{io: :stdio}
+    assert Patrol.eval("IO.puts('Hello world from Patrol!')", sb)
+  end
+
+  test "redirect IO to a file" do
+       io_file = "test/fixtures/stdio.txt"
+
+       sb = %Patrol.Sandbox{io: File.open!(io_file, [:write, :read])}
+       msg = "Hello world from Patrol!"
+       {:ok, :ok} = Patrol.eval("IO.puts('#{msg}')", sb)
+       contents = File.read!(io_file)
+       assert contents = msg
+  end
+
 end
