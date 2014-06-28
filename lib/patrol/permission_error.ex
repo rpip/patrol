aa25a0f00fea2d3bf01b21bf13289b4b4776fac1
Mawuli Adzaku <mawuli@mawuli.me>
2014-06-28 01:47:53 +0000

    Minor refactoring of sandbox exception

--- lib/patrol/permission_error.ex
+++ lib/patrol/permission_error.ex
@@ -2,10 +2,11 @@ defmodule Patrol.PermissionError do
   @moduledoc """
   Exception to be thrown when a code contains a restricted code
   """
+
   defexception [:message]
 
-  def exception(value) do
-    msg = "You tripped the alarm! \n #{Macro.to_string value} is not allowed"
+  def exception(code) do
+    msg = "You tripped the alarm! \n #{code} is not allowed"
     %Patrol.PermissionError{message: msg}
   end
 end
